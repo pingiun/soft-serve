d8fd50aada6aa0fadaecbace1bf6b7af1899b8fd
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-10-18 15:22:12 -0400

    feat(config): add repo collabs
    A repo config now can take a list of collab user names.

--- config/auth.go
+++ config/auth.go
@@ -114,6 +114,11 @@ OUT:
 		if anon > ac {
 			ac = anon
 		}
+		for _, c := range r.Collabs {
+			if c == u.Name {
+				return ac
+			}
+		}
 		for _, rr := range u.CollabRepos {
 			if rr == r.Repo {
 				return ac
